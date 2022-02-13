24cd566af856f6b00b208e8fd5f4cbdc235d7811
xxxx <coulibalyissa956@gmail.com>
2022-02-13 02:33:43 +0000

    guessing game finished

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Devinez le nombre !");
-    println!("Veuillez entrer un nombre");
+    
+    let nombre_secret = rand::thread_rng().gen_range(1..11);
+    
+    loop {
+        println!("Veuillez entrer un nombre");
 
-    let mut supposition = String::new();
+        let mut supposition = String::new();
 
-    io::stdin()
-        .read_line(&mut supposition)
-        .expect("Echec de la lecture de l'entrée utilisateur");
+        io::stdin()
+            .read_line(&mut supposition)
+            .expect("Echec de la lecture de l'entrée utilisateur");
 
-    println!("Votre nombre: {}", supposition);
+        println!("Votre nombre: {}", supposition);
+
+        let supposition: u32 = match supposition.trim().parse() {
+            Ok(nombre) => nombre,
+            Err(_) => continue,
+        };
+
+        match supposition.cmp(&nombre_secret) {
+            Ordering::Less => println!("C'est plus"),
+            Ordering::Greater => println!("C'est moins"),
+            Ordering::Equal => {
+                println!("Vous avez gagné !");
+                break;
+            }
+        }
+    }
 }
